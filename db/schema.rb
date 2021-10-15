9400fea1042b80139de41997bacbf028f93671c3
eakkuzey <ensar@tursys.com>
2021-10-15 17:36:22 +0300

    Change db types.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_10_14_215625) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "title"
     t.text "content"
