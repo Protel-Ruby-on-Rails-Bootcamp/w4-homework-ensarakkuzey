22ace663beaf9c7380745f4c21ad411f72ca7c67
Süleyman Ensar Akkuzey <ensarakkuzey@outlook.com>
2021-10-20 00:51:52 +0300

    Add follow user structure.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,31 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  attr_writer :login
 
   has_many :posts
   has_many :comments
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :following, through: 'active_relationships', source: :followed
+
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followers, through: 'passive_relationships', source: :followed
 
   validates :username, presence: true, length: { minimum: 5, maximum: 50 }
-
   validate :validate_username
 
-  attr_writer :login
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 
   def validate_username
     self.errors.add(:username, :invalid) if User.where(email: username).exists?
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :relationships, only: [:create, :destroy] 
+
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
