f6ce380070b7b8cea5359e1bbf340ccb4ec66e55
Süleyman Ensar Akkuzey <ensarakkuzey@outlook.com>
2021-10-26 22:10:36 +0300

    Add gravatar gem, design profile page.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,13 @@ class Post < ApplicationRecord
     validates :content, presence: true
 
     belongs_to :user
-    has_many :comments
+    has_many :comments, dependent: :destroy
 
     scope :availables, -> { where(visible: true) }
+
+    def reading_time
+        words_per_minute = 150
+        text = Nokogiri::HTML(self.content).at('body').inner_text
+        (text.scan(/\w+/).length / words_per_minute).to_i + 1
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  include Gravtastic
+  gravtastic
   
   attr_writer :login
 
